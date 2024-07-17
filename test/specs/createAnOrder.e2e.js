2d64a2a06dba17934a33c26aeda63086a5cfdf31
maor <maorshaer87@gmail.com>
2024-07-17 19:28:12 +0300

    my first commit

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -19,5 +19,76 @@ describe('Create an order', () => {
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
     })
-})
+
+     it('should enter the address', async () => {
+        await browser.url('/');
+
+        // Locate the "from" address input field and set its value
+        const fromField = await $('#from');
+        await fromField.setValue('East 2nd Street, 601');
+        expect(await fromField.getValue()).toBe('East 2nd Street, 601');
+
+        // Locate the "to" address input field and set its value
+        const toField = await $('#to');
+        await toField.setValue('1300 1st St');
+        expect(await toField.getValue()).toBe('1300 1st St');
+    });
+
+     it('Taxi Plan Selection', async () => {
+        await browser.url('/');
+
+        // this class: 'tcard' selects all taxi plans different types of taxi's
+        const allTaxiPlans = await $('.tcard');
+        await allTaxiPlans.click();
+
+        // Verify taxi plan, the message popup
+        const confirmMessagePopup = await $('.order-body');
+        expect(confirmMessagePopup).toBeDisplayed();
+     });
+
+     it('Filling in the phone number, input field', async () => {
+        await browser.url('/');
+
+        // Fill the input with phone number
+        const fillNumber = await $('#phone');
+        await fillNumber.setValue('+12345678907');
+
+        // Verify the phone number entered correctly
+        const enteredValue = await fillNumber.getValue();
+        expect(enteredValue).toBe('+12345678907');
+     });
+
+     it('Credit Card number entry', async () => {
+        await browser.url('/');
+
+        // Fill the credit card number field
+        const cardNumberInput = await $('.card-number');
+        await cardNumberInput.setValue('123400005678');
+
+        // Fill the cvv input of credit card
+        const cvvInput = await $('.card-input');
+        await cvvInput.setValue('19');
+
+        // Submit button of credit card named: 'Link'
+        const submitButton = await $('.button full');
+        await submitButton.click();
+
+        // Cancel button of credit card named: 'Cancel'
+        const cancelButton = await $('//button[contains(text(), "Cancel")]');
+        await cancelButton.click();
+     });
+
+     it('Write a message for the driver input', async () => {
+        await browser.url('/');
+
+        // Input field and enter a message to driver
+        const messageDriverInput = await $('#comment');
+        const messageDriver = 'get me coffee please';
+        await messageDriverInput.setValue(messageDriver);
+
+        // Verify the message was entered correctly
+        const enterdMessage = await messageDriverInput.getValue();
+        expect(enterdMessage).toBe(messageDriver);
+     });
+});
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -7,14 +7,14 @@ exports.config = {
         // 'path/to/excluded/files'
     ],
     maxInstances: 10,
-    headless: true,
+    headless: false,
     capabilities: [
         {
         maxInstances: 5,
         browserName: 'chrome',
         acceptInsecureCerts: true,
         'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
+            //args: ['headless', 'disable-gpu']
         }
     },
     {
@@ -22,13 +22,13 @@ exports.config = {
         browserName: 'firefox',
         acceptInsecureCerts: true,
         'moz:firefoxOptions': {
-            args: ['-headless']
+            //args: ['-headless']
         }
     }
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://e629ed6b-4969-442b-987e-087e81eb10ca.serverhub.practicum-services.com',
+    baseUrl: 'https://cnt-1a44cfb6-7044-487e-9d86-58a9e5bd816f.containerhub.tripleten-services.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
